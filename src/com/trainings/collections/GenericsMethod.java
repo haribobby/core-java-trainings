6dae14291972e48f11865e595a80bb0ef779b640
phariven <harivenkatesh_polnati@optum.com>
2021-09-24 15:45:34 +0530

    arbitrary generics

--- src/com/trainings/collections/GenericsMethod.java
+++ src/com/trainings/collections/GenericsMethod.java
@@ -1,6 +1,5 @@
 package com.trainings.collections;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,36 +7,28 @@ public class GenericsMethod {
 
 	public static void main(String[] args) {
 
-		System.out.println(getList());
+		System.out.println(getList(List.of("Apple", "Banana", "Orange")));
+
+		System.out.println(getListUpperBound(List.of("Apple1", "Banana1", "Orange1")));
 	}
 
-//	static List<? extends Number> getList(List<? extends Number> list) {
-//
-//
-//		
-//		return list;
-//	}
-	
-	
-	static <T> List<T> getList() {
-		
-		
-		
-		
-		List<String> list = new ArrayList<String>();
-		list.add("sdfsf");
-		
-		
-		return (List<T>) list; 
-		
-		
-		
-		
-		
+	static List<? extends String> getListUpperBound(List<? extends String> list) {
+
+		return list;
 	}
-		
+
+	static <T> List<T> getList(List<T> argList) {
+
+		List<T> list = new ArrayList<T>();
+		for (T element : argList) {
+			list.add(element);
+		}
+		return list;
+
+	}
+
+	// line 20: <T> means We are saying to compiler this is an arbitrary type which can be any object.
+	// line 20: List<T> => getList method will return a List containing elements of that type T
+
 	
 }
-
-
-
