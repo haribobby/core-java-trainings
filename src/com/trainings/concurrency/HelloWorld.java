0f9ba0139047f2306aa682db78feb5da579693db
phariven <harivenkatesh_polnati@optum.com>
2021-10-19 11:53:10 +0530

    Concurrency API starts

---
+++ src/com/trainings/concurrency/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.trainings.concurrency;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println(Thread.currentThread().getName());
+		
+		System.out.println("Hello Java!!!");
+		
+		
+		
+		
+	}
+
+}
---
+++ src/com/trainings/puzzles/OverloadMain.java
@@ -0,0 +1,16 @@
+package com.trainings.puzzles;
+
+public class OverloadMain {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("In String[] args");
+	}
+
+	public static void main(String args) {
+
+		System.out.println("In String args");
+	}
+
+}
